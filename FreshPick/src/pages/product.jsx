9cefe05930d5bb6dde1da2592f1ac1701cca8ba9
tjqjatlr <112400043+tjqjatlr@users.noreply.github.com>
2024-05-28 16:50:25 +0900

    Delete FreshPick directory
    파일 재 업로드

--- FreshPick/src/pages/product.jsx
+++
@@ -1,7 +0,0 @@
- import { Detail } from "../components/product_detail/detail";
-
-const Product = ({convertPrice}) => {
-  return <Detail convertPrice = {convertPrice}/>;
-};
-
-export default Product;
